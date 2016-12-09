27b930baba260e76051e83c72df19b7c39081175
Matt Berteaux <mberteaux@ancestry.com>
2016-12-08 20:49:00 -0700

    Finished with day 2 part 2

--- src/aoc/day2.clj
+++ src/aoc/day2.clj
@@ -6,6 +6,12 @@
               [4 5 6]
               [7 8 9]])
 
+(def big-pad [[nil nil \1 nil nil]
+              [nil \2 \3 \4 nil]
+              [\5 \6 \7 \8 \9]
+              [nil \A \B \C nil]
+              [nil nil \D nil nil]])
+
 (defn -move [pad [row col] dir]
   (let [new-pos (case dir
                   \U [(dec row) col]
@@ -16,12 +22,12 @@
       new-pos
       [row col])))
 
-(defn -part1 [lines]
-  (str/join (map (partial get-in num-pad)
-       (loop [lns lines pos [1 1] res []]
+(defn -process [lines pad start]
+  (str/join (map (partial get-in pad)
+       (loop [lns lines pos start res []]
     (if (empty? lns)
       res
-      (let [new-pos (reduce (partial -move num-pad) pos (first lns))]
+      (let [new-pos (reduce (partial -move pad) pos (first lns))]
         (recur (rest lns)
                new-pos
                (conj res new-pos))))))))
@@ -29,4 +35,5 @@
 (defn challenge []
   (println "Day 2")
   (let [lines (str/split-lines (utils/input-str "day2.txt"))]
-   (println "Part 1: " (-part1 lines)) ))
+   (println "Part 1: " (-process lines num-pad [1 1]))
+   (println "Part 2: " (-process lines big-pad [2 0]))))
